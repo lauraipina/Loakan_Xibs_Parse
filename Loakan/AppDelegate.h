0d79a99c725c46ca8a215cf6bee661ecda1299c9
Laura Iglesias <laura.iglesias.pina@gmail.com>
2015-03-04 17:54:29 +0100

    Initial Commit

---
+++ Loakan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Loakan
+//
+//  Created by Laura Iglesias Piña on 4/3/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
